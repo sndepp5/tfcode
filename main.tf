f156df5c1b8b4256483668836818fe81c5cacd23
CCS2BAN <ccs2ban@bosch.com>
2023-08-04 10:58:30 +0530

    terrascan

--- main.tf
+++ main.tf
@@ -28,6 +28,16 @@ resource "aws_s3_bucket" "logbucket" {
     bucket = "my-logg-cc"
 }
 
+resource "aws_s3_bucket_public_access_block" "accessblklog" {
+    bucket = aws_s3_bucket.logbucket.id
+
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+
+}
+
 resource "aws_s3_bucket_acl" "logacl" {
     bucket = aws_s3_bucket.logbucket.id
     acl = "log-delivery-write"
