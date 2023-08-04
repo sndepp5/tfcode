f3dd4bb143b20121fcac4f30add56ea48b436796
CCS2BAN <ccs2ban@bosch.com>
2023-08-04 10:46:46 +0530

    addon

--- main.tf
+++ main.tf
@@ -19,3 +19,23 @@ resource "aws_s3_bucket_public_access_block" "accessblk" {
 
 }
 
+resource "aws_s3_bucket_acl" "bacl" {
+    bucket = aws_s3_bucket.bucket.id
+    acl = "private"
+}
+
+resource "aws_s3_bucket" "logbucket" {
+    bucket = "my-logg-cc"
+}
+
+resource "aws_s3_bucket_acl" "logacl" {
+    bucket = aws_s3_bucket.logbucket.id
+    acl = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_logging" "logexp" {
+  bucket = aws_s3_bucket.bucket.id
+
+  target_bucket = aws_s3_bucket.logbucket.id
+  target_prefix = "log/"
+}
