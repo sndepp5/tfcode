837c6d726beb9cba45d457f558f1c2fdaf8bad49
CCS2BAN <ccs2ban@bosch.com>
2023-08-16 12:36:40 +0530

    init

--- providers.tf
+++ providers.tf
@@ -2,10 +2,18 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.10.0"
+      version = ">=5.10.0"
     }
   }
-}
+
+  backend "s3" {
+    bucket         = "terraform-up-and-running-statezpl-ccs"
+    dynamodb_table = "terraform-up-and-running-locks-ccs"
+    key            = "global/terraform.tfstate"
+    region         = "eu-central-1"
+    profile        = "default" 
+  } 
+} 
 
 provider "aws" {
   region = var.region
