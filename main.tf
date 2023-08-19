3be2f3b084ea2892433d5d676a68d28d9851450b
CCS2BAN <ccs2ban@bosch.com>
2023-08-19 15:21:30 +0530

    old

--- main.tf
+++ main.tf
@@ -72,4 +72,12 @@ resource "aws_s3_bucket" "bucket9" {
     }
 }
 
+resource "aws_vpc" "vpc" {
+  cidr_block              = "10.0.0.0/16"
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
 
+  tags      = {
+    Name    = "DemoVPC"
+  }
+}
