d9873430c338b243e4bd773752388ffed3184e86
CCS2BAN <ccs2ban@bosch.com>
2023-08-24 11:20:38 +0530

    mail

--- main.tf
+++ main.tf
@@ -71,13 +71,3 @@ resource "aws_s3_bucket" "bucket9" {
         CreatedBy = "Sandeep"
     }
 }
-
-resource "aws_vpc" "vpc" {
-  cidr_block              = "10.0.0.0/16"
-  enable_dns_hostnames    = true
-  enable_dns_support      = true
-
-  tags      = {
-    Name    = "DemoVPC"
-  }
-}
