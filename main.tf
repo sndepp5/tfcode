42cf45fd0c778fc25b0ce69bb9f923d2f01c2813
CCS2BAN <ccs2ban@bosch.com>
2023-09-12 12:10:42 +0530

    tfs3

--- main.tf
+++ main.tf
@@ -1,75 +1,75 @@
-# #terraform pls
+#terraform pls
 
 
-# resource "aws_s3_bucket" "bucket" {
-#     bucket = "ccs-test-terraform-ccs369"
-#     tags = {
-#         Name = "Buc"
-#         CreatedBy = "Sandeep"
-#     }
-# }
+resource "aws_s3_bucket" "bucket" {
+    bucket = "ccs-test-terraform-ccs369"
+    tags = {
+        Name = "Buc"
+        CreatedBy = "Sandeep"
+    }
+}
 
-# resource "aws_s3_bucket_public_access_block" "accessblk" {
-#     bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket_public_access_block" "accessblk" {
+    bucket = aws_s3_bucket.bucket.id
 
-#     block_public_acls = true
-#     block_public_policy = true
-#     ignore_public_acls = true
-#     restrict_public_buckets = true
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
 
-# }
-
-
-# resource "aws_s3_bucket" "logbucket" {
-#     bucket = "my-logg-cc-test-ccs369"
-# }
-
-# resource "aws_s3_bucket_public_access_block" "accessblklog" {
-#     bucket = aws_s3_bucket.logbucket.id
-
-#     block_public_acls = true
-#     block_public_policy = true
-#     ignore_public_acls = true
-#     restrict_public_buckets = true
-
-# }
+}
 
 
-# resource "aws_s3_bucket_logging" "logexp" {
-#   bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket" "logbucket" {
+    bucket = "my-logg-cc-test-ccs369"
+}
 
-#   target_bucket = aws_s3_bucket.logbucket.id
-#   target_prefix = "log/"
-# }
+resource "aws_s3_bucket_public_access_block" "accessblklog" {
+    bucket = aws_s3_bucket.logbucket.id
 
-# resource "aws_s3_bucket_versioning" "versioning_example" {
-#   bucket = aws_s3_bucket.bucket.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
 
-# resource "aws_s3_bucket_versioning" "versioning_example1" {
-#   bucket = aws_s3_bucket.logbucket.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+}
 
-# resource "aws_s3_bucket" "bucket3" {
-#     bucket = "ccs-terraform-ccs36999"
-#     tags = {
-#         Name = "Buck"
-#         CreatedBy = "Sandeep"
-#     }
-# }
 
-# resource "aws_s3_bucket" "bucket9" {
-#     bucket = "tesla-sndepp"
-#     tags = {
-#         Name = "Buck"
-#         CreatedBy = "Sandeep"
-#     }
-# }
+resource "aws_s3_bucket_logging" "logexp" {
+  bucket = aws_s3_bucket.bucket.id
+
+  target_bucket = aws_s3_bucket.logbucket.id
+  target_prefix = "log/"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example1" {
+  bucket = aws_s3_bucket.logbucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "bucket3" {
+    bucket = "ccs-terraform-ccs36999"
+    tags = {
+        Name = "Buck"
+        CreatedBy = "Sandeep"
+    }
+}
+
+resource "aws_s3_bucket" "bucket9" {
+    bucket = "tesla-sndepp"
+    tags = {
+        Name = "Buck"
+        CreatedBy = "Sandeep"
+    }
+}
 
 
